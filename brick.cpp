9b120ff986b71ce79c9d749efcc66590e33cb0eb
alenahalm <alenahalmatova749@gmail.com>
2020-12-16 02:35:45 +0800

    Initial commit

---
+++ brick.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double minb, middle, maxb;
+	cout << "Enter a, b, c" << endl;
+	cin >> minb >> middle >> maxb;
+	double l, m;
+	cout << "Enter r and s" << endl;
+	cin >> l >> m;
+	if (m < l) {
+		swap(m, l);
+	}
+	if (middle > maxb) {
+		swap(middle, maxb);
+	}
+	if (minb > maxb) {
+		swap(minb, maxb);
+	}
+	if (minb > middle) {
+		swap(minb, middle);
+	}
+	if (minb <= l && middle <= m) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+	system("pause");
+}
+void swap(double a, double b) {
+	double x = a;
+	a = b;
+	b = x;
+}
